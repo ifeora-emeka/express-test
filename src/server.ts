e9869020678767ddd1006a25baf2e5d9f3f1e657
chukwuemekaifeora <chukwuemekaifeora@gmail.com>
2025-03-31 23:41:16 +0000

    added docker

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import mongoose from 'mongoose';
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI || '';
 
+console.log('MONGO DB URI::', MONGO_URI)
+
 mongoose
     .connect(MONGO_URI)
     .then(() => {
